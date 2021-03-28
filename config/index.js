10e1b0c9e0305e1c09ac2c36f09821950a156c58
lisindima <lisindima1996@gmail.com>
2021-03-28 14:28:09 +0700

    bump

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   siteUrl: "https://lisindmitriy.me/",
   siteLanguage: "en_US",
   siteIcon: "content/favicon.png", // Relative to gatsby-config file
-  seoTitleSuffix: "Portfolio Minimal", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
+  seoTitleSuffix: "Lisin Dmitriy", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
   useCookieBar: false, // If you use Google Analytics and want to be GDPR-compliant, set it to true
   googleAnalyticsTrackingId: "", // e.g. UA-XXXXXX-X
 
