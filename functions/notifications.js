e44bd43e016a189b48a6958dee5c52da07391a86
Lisin Dmitriy <lisindima1996@gmail.com>
2021-05-13 17:25:26 +0700

    Update notifications.js

--- functions/notifications.js
+++ functions/notifications.js
@@ -1,19 +1,16 @@
 exports.handler = function (event, context, callback) {
 
     const device_id = event.queryStringParameters.device_id
-    const body = event.body
 
-    let deploy = {} 
-
-    try {
-        deploy = JSON.parse(body)
-      } catch (e) {
-        console.log(e)
-      }
+    // try {
+    //   deploy = JSON.parse(event.body)
+    // } catch (e) {
+    //   console.log(e)
+    // }
 
 
     callback(null, {
       statusCode: 200,
-      body: deploy,
+      body: JSON.parse(event.body),
     });
   };
