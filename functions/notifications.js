c184c341e1974197bf23e7d8cb71ef048a07222b
Lisin Dmitriy <lisindima1996@gmail.com>
2021-05-13 18:20:05 +0700

    bump

--- functions/notifications.js
+++ functions/notifications.js
@@ -1,11 +1,37 @@
 exports.handler = function (event, context, callback) {
 
+    const apn = require("apn");
+
     const device_id = event.queryStringParameters.device_id
     const name_site = event.queryStringParameters.name_site
     const type = event.queryStringParameters.type
 
+    var options = {
+        token: {
+          key: "functions/AuthKey_L3F379QHSL.p8",
+          keyId: "L3F379QHSL",
+          teamId: "48HFZR3X8K"
+        },
+        production: false
+    };
+      
+    var apnProvider = new apn.Provider(options);
+
+    var note = new apn.Notification();
+
+    note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+    note.badge = 3;
+    note.sound = "ping.aiff";
+    note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+    note.payload = {'messageFrom': 'John Appleseed'};
+    note.topic = "<your-app-bundle-id>";
+
+    apnProvider.send(note, "51c1238490bddaf8aa1812b33e7b53825af1a9046aec00b7acf4cb5b29b6cb68").then( (result) => {
+        // see documentation for an explanation of result
+    });
+
     callback(null, {
       statusCode: 200,
-      body: `Notification sent: ${device_id}`,
+      body: `Notification sent: 51c1238490bddaf8aa1812b33e7b53825af1a9046aec00b7acf4cb5b29b6cb68`,
     });
   };
