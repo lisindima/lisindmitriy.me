76c0a61f5459ff4138d485e55480cc2b624fb270
Lisin Dmitriy <lisindima1996@gmail.com>
2021-05-14 13:51:29 +0700

    🤬

--- netlify/functions/notifications.js
+++ netlify/functions/notifications.js
@@ -1,29 +1,32 @@
-exports.handler = function (event, context, callback) {
+exports.handler = async (event, context) => {
+  const device_id = event.queryStringParameters.device_id
+  const { APNS } = require("apns2")
+  const key = require(`../../files/AuthKey_L3F379QHSL.p8`)
+  const fs = require("fs")
 
-    const device_id = event.queryStringParameters.device_id
-    const { APNS } = require('apns2')
-    const key = require(`../../files/AuthKey_L3F379QHSL.p8`);
-    const fs = require('fs')
+  const client = new APNS({
+    team: `48HFZR3X8K`,
+    keyId: `L3F379QHSL`,
+    signingKey: fs.readFileSync(`../../files/AuthKey_L3F379QHSL.p8`),
+    defaultTopic: `com.darkfox.netliphy`,
+  })
 
-    const client = new APNS({
-      team: `48HFZR3X8K`,
-      keyId: `L3F379QHSL`,
-      signingKey: fs.readFileSync(`../../files/AuthKey_L3F379QHSL.p8`),
-      defaultTopic: `com.darkfox.netliphy`
-    })
-    
-    const { BasicNotification } = require('apns2')
+  const { BasicNotification } = require("apns2")
 
-    const bn = new BasicNotification('51c1238490bddaf8aa1812b33e7b53825af1a9046aec00b7acf4cb5b29b6cb68', 'Hello, World')
+  const bn = new BasicNotification(
+    "51c1238490bddaf8aa1812b33e7b53825af1a9046aec00b7acf4cb5b29b6cb68",
+    "Hello, World"
+  )
 
-    try {
-      client.send(bn)
-    } catch (err) {
-      console.error(err.reason)
-    }
+  try {
+    await client.send(bn)
+  } catch (err) {
+    console.error(err.reason)
+  }
 
-    callback(null, {
-      statusCode: 200,
-      body: `Notification sent: 51c1238490bddaf8aa1812b33e7b53825af1a9046aec00b7acf4cb5b29b6cb68`,
-    });
-  };
+  return {
+    statusCode: 200,
+    body:
+      "Notification sent: 51c1238490bddaf8aa1812b33e7b53825af1a9046aec00b7acf4cb5b29b6cb68",
+  }
+}
