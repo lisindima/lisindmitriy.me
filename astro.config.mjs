8db2b7893e3e52f350a52b55eeeba534382b35db
Lisin Dmitriy <lisindima1996@gmail.com>
2026-09-20 07:03:02 +0300

    Externalize CSS for ClientRouter transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://lisindmitriy.ru",
   build: {
-    inlineStylesheets: "always",
+    inlineStylesheets: "never",
   },
   security: {
     csp: {
