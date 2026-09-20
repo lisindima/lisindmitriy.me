b5b027d37a342a69545621f8ee51b42bd91f2b20
Lisin Dmitriy <lisindima1996@gmail.com>
2026-09-20 11:39:58 +0300

    Redirect legacy project routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,14 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://lisindmitriy.ru",
+  redirects: {
+    "/projects/netliphy/": "/netliphy/",
+    "/projects/otphub/": "/otphub/",
+    "/projects/covid-dashboard/": "/covid-dashboard/",
+    "/en/projects/netliphy/": "/en/netliphy/",
+    "/en/projects/otphub/": "/en/otphub/",
+    "/en/projects/covid-dashboard/": "/en/covid-dashboard/",
+  },
   build: {
     inlineStylesheets: "never",
   },
