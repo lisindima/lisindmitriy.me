ef318e8baed382e8a6c82e1e8b744dc053ad3028
Lisin Dmitriy <lisindima1996@gmail.com>
2026-09-24 20:53:15 +0300

    Centralize site route transitions

---
+++ src/lib/siteTransitions.ts
@@ -0,0 +1,5 @@
+import { fade } from "astro:transitions";
+
+export const siteHeaderTransition = () => fade({ duration: "0.18s" });
+export const siteContentTransition = () => fade({ duration: "0.22s" });
+export const siteFooterTransition = () => fade({ duration: "0.22s" });
