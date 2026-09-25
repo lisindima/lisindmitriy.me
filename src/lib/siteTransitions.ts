2ae08cd3d95c9b8b23a5845269c289baa1371358
Lisin Dmitriy <lisindima1996@gmail.com>
2026-09-25 10:29:05 +0300

    Remove unused header transition helper

--- src/lib/siteTransitions.ts
+++ src/lib/siteTransitions.ts
@@ -1,4 +1,3 @@
 import { fade } from "astro:transitions";
 
-export const siteHeaderTransition = () => fade({ duration: "0.18s" });
 export const siteContentTransition = () => fade({ duration: "0.22s" });
